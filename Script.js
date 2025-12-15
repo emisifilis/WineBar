2aaa576d31977a0bdd62948b1a47beb85aac2e3f
emisifilis <riosortizemiliano@gmail.com>
2025-12-15 03:03:03 -0600

    Made the header beterrrr now it is transparent at the beggining and materialise when scrolling also hero complited just need a fucking ggood picture of the bar

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 
-let lastScrollTop = 0; 
+
 
 const SCROLL_SENSITIVITY = 5; 
 
@@ -10,30 +10,26 @@ window.onscroll = function() {
 function handleScrollDirection() {
     const headerContainer = document.getElementsByClassName("HeaderContainer")[0];
     const VHaPX = window.innerHeight * (4 / 100);
-    
+    const imageElement1 = document.getElementById("Facebook");
+    const imageElement2 = document.getElementById("Instagram");
+    const imageElement3 = document.getElementById("TripAdvisor");
     
     const currentScrollTop = document.documentElement.scrollTop;
 
     
-    if (currentScrollTop < VHaPX) {
-        headerContainer.classList.remove("scrolled");
-        lastScrollTop = currentScrollTop;
-        return; 
-    }
-
     
-    if (currentScrollTop > lastScrollTop + SCROLL_SENSITIVITY) {
+    if(currentScrollTop < VHaPX){
+        headerContainer.classList.remove("scrolled");
+        imageElement1.src = "/ASSETS/image (1).webp"
+        imageElement2.src = "/ASSETS/image (2).webp"
+        imageElement3.src = "/ASSETS/image (3).webp"
         
-        
+    }else{
         headerContainer.classList.add("scrolled");
-        
-    } else if (currentScrollTop < lastScrollTop - SCROLL_SENSITIVITY) {
-        
-        
-        headerContainer.classList.remove("scrolled");
-        
+        imageElement1.src = "/ASSETS/facebook-app-symbol.png"
+        imageElement2.src = "/ASSETS/instagramicon.png"
+        imageElement3.src = "/ASSETS/triptri.png"
     }
-
     
-    lastScrollTop = currentScrollTop;
+    
 }
